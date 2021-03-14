11b95e330190c319592fd9b5a1f8f71f3668dd38
shangjingfan <876625909@qq.com>
2021-03-14 13:52:11 +0800

    完成组件Nav.tsx的icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-import x from 'icons/money.svg';
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -11,7 +13,14 @@ const NavWrapper = styled.nav`
     > li{
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,13 +30,21 @@ const Nav = ()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className={"icon"}>
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
-          <img src={x} alt=""/>
+          <svg className={"icon"}>
+            <use xlinkHref={"#tag"}/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className={"icon"}>
+            <use xlinkHref={"#chart"}/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
