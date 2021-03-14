53c3139e6ebe89f10740637a0d9814dade3abdb7
shangjingfan <876625909@qq.com>
2021-03-14 12:24:14 +0800

    完成导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,95 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
+const Wrapper = styled.div`
+  border: 1px solid red;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  
+`;
+const Main = styled.div`
+  border: 1px solid blue; 
+  flex-grow: 1;
+  overflow: auto;
+ `;
+const Nav = styled.nav`
+  border: 1px solid green;
+  > ul{
+    display: flex;
+    > li{
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+
+export default function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h3>
+        404：页面不存在，你输错了
+      </h3>
+    </div>
+  );
+}
