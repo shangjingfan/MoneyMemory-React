180918b1fbdc755f2448c8e9c65190aa0f17f14d
shangjingfan <876625909@qq.com>
2021-03-15 13:18:44 +0800

    完成备注的功能，受控组件和非受控组件

---
+++ src/views/Money/NoteSection.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import React, {useRef, useState} from 'react';
+
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label{
+    display: flex;
+    align-items: center;
+    > span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input{
+      //flex-grow: 1;
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+
+const NoteSection: React.FC = () => {
+  const [note, setNote] = useState('');
+  console.log(note);
+  const refInput = useRef<HTMLInputElement>(null);
+  const onBlur = () => {
+    if (refInput.current !== null) {
+      setNote(refInput.current.value);
+    }
+  };
+  return (
+    <Wrapper>
+      <label>
+        <span>备注</span>
+        <input type="text" placeholder="在这里添加备注"
+            // 受控组件
+            // value={note}
+            // onChange={(e)=> setNote(e.target.value)}
+            // 非受控组件
+               ref={refInput}
+               defaultValue={note}
+               onBlur={onBlur}
+        />
+      </label>
+    </Wrapper>
+  );
+};
+export {NoteSection};
