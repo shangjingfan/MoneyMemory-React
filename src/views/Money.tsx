e62ea33ed94117ba15ffaea3b80fb34c40ace186
shangjingfan <876625909@qq.com>
2021-03-15 10:04:53 +0800

    Money.tsx的TagSection完成样式

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,30 +4,36 @@ import styled from 'styled-components';
 
 const TagsSection = styled.section`
   background: #FFFFFF;
+  padding: 12px 16px;
+  > ol{ margin: 0 -12px;
+    > li{
+      background: #D9D9D9; border-radius: 18px;
+      display: inline-block; padding: 3px 18px;
+      font-size: 14px; margin: 8px 12px;
+    }
+  }
+  > button{
+    background: none; border: none;
+    border-bottom: 1px solid #333;
+    padding: 2px 4px; color: #666;
+    margin-top: 8px;
+  }
 `;
+
 const NotesSection = styled.section``;
 const CategorySection = styled.section``;
 const NumberPadSection = styled.section``;
-const TagList = styled.ol`
-  > li{
-    background: #D9D9D9;
-    border-radius: 18px;
-    display: inline-block;
-    padding: 3px 18px;
-    font-size: 14px;
-  }
-`;
 
 function Money() {
   return (
     <Layout>
       <TagsSection>
-        <TagList>
+        <ol>
           <li>衣</li>
           <li>食</li>
           <li>住</li>
           <li>行</li>
-        </TagList>
+        </ol>
         <button>新增标签</button>
       </TagsSection>
       <NotesSection>
