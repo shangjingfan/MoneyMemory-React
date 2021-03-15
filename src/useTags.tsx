dae7995c5cc85b28966b66367d70d916115bbb11
shangjingfan <876625909@qq.com>
2021-03-15 23:52:47 +0800

    创建lib/createId.tsx，让id自增

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react';
+import {createId} from 'lib/createId';
 type Tag = {
   id: number;
   name: string
 }
 const useTags = () => {
   const [tags, setTags] = useState<Tag[]>([
-    {id: 1, name: '衣'},
-    {id: 2, name: '食'},
-    {id: 3, name: '住'},
-    {id: 4, name: '行'}
+    {id: createId(), name: '衣'},
+    {id: createId(), name: '食'},
+    {id: createId(), name: '住'},
+    {id: createId(), name: '行'}
   ]);
   return {
     tags,
