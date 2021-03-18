dddca95f9382f83a3576c621595648bc3cf14ba3
shangjingfan <876625909@qq.com>
2021-03-18 16:27:21 +0800

    change name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:shangjingfan/MoneyMemory-react-web.git &&
+git remote add origin git@github.com:shangjingfan/MoneyMemory-React-website.git &&
 git push -u origin master -f
 cd -
